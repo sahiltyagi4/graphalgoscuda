c0db1b8ad60beb64d5e268052178202e374ea005
sahiltyagi4 <sahilt.tyagi@gmail.com>
2022-04-26 14:16:24 -0400

    cuda with stride implementation

--- cuda/cudastride_sssp.cu
+++ cuda/cudastride_sssp.cu
@@ -152,7 +152,7 @@ int main(){
     initialize<<<init_blocks, threads_per_block>>>(V.size(), d_out_Di, max_value, true, 0, 0);
     initialize<<<init_blocks, threads_per_block>>>(V.size(), d_out_P, max_value, true, 0, 0);
 
-    update_INDEX_edges<<<init_blocks, threads_per_block>>>(E.size(), d_in_V, d_in_E, 0, V.size()-1);
+    update_INDEX_edgesGridStride<<<init_blocks, threads_per_block>>>(E.size(), d_in_V, d_in_E, 0, V.size()-1);
     for (int i=0; i<V.size(); ++i){
         resetGridStride()<<<init_blocks, threads_per_block>>>(N, max_value, d_in_V, d_in_I, d_in_E, d_in_W, d_out_D, d_out_Di);
         update_distanceGridStride<<<blocks, threads_per_block>>>(V.size(), d_in_V, d_in_I, d_in_E, d_in_W, d_out_D, d_out_Di);
