138490bb831b0f2ffe6a3a12cc63282e269a9ea4
sahiltyagi4 <sahilt.tyagi@gmail.com>
2022-04-26 14:00:54 -0400

    cuda with stride implementation

--- cuda/cudastride_sssp.cu
+++ cuda/cudastride_sssp.cu
@@ -26,12 +26,26 @@ void load_data(const char *f, std::vector<int> &vec){
     inputfile.close();
 }
 
-__global__ void reset(int N, int MAX_VAL, int *d_in_V, int *d_in_I, int *d_in_E, int *d_in_W, int *d_out_D, int *d_out_Di){
+__global__ void initialize(int N, int *p, int val, bool src, int source, int sourceVal){
+    int ix = threadIdx.x + blockDim.x * blockIdx.x;
+    int stride = gridDim.x * blockDim.x;
+    for (int i=ix; i<N; i+= stride){
+        p[i] = val;
+        if(sourceDifferent){
+            if(index == source) {
+                p[i] = sourceVal;
+            }
+        }
+    }
+}
+
+__global__ void resetGridStride(int N, int MAX_VAL, int *d_in_V, int *d_in_I, int *d_in_E, int *d_in_W, int *d_out_D, int *d_out_Di){
     int ix = threadIdx.x + blockIdx.x * blockDim.x;
-    if (ix < N-1){
-        for (int j = d_in_I[ix]; j < d_in_I[ix + 1]; j++) {
+    int stride = gridDim.x * blockDim.x;
+    for (int i=ix; i< N-1; i+=stride){
+        for (int j = d_in_I[i]; j < d_in_I[i + 1]; j++) {
             int w = d_in_W[j];
-            int du = d_out_D[ix];
+            int du = d_out_D[i];
             int dv = d_out_D[d_in_E[j]];
             int newDist = du + w;
             if (du == MAX_VAL){
@@ -45,49 +59,52 @@ __global__ void reset(int N, int MAX_VAL, int *d_in_V, int *d_in_I, int *d_in_E,
     }
 }
 
-__global__ void update_distance(int N, int *d_in_V, int *d_in_I, int *d_in_E, int *d_in_W, int *d_out_D, int *d_out_Di){
+__global__ void update_distanceGridStride(int N, int *d_in_V, int *d_in_I, int *d_in_E, int *d_in_W, int *d_out_D, int *d_out_Di){
     int ix = threadIdx.x + blockIdx.x * blockDim.x;
-    if (ix < N) {
-        if (d_out_D[ix] > d_out_Di[ix]) {d_out_D[ix] = d_out_Di[ix];}
-        d_out_Di[ix] = d_out_D[ix];
+    int stride = gridDim.x * blockDim.x;
+    for (int i=ix; i< N-1; i+=stride){
+        if (d_out_D[i] > d_out_Di[i]) {
+            d_out_D[i] = d_out_Di[i];
+        }
+        d_out_Di[i] = d_out_D[i];
     }
 }
 
-__global__ void update_INDEX_edges(int N, int *d_in_V, int *d_in_E, int l, int r){
-    int ix = threadIdx.x + blockDim.x * blockIdx.x;
-    if (ix < N) {
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (d_in_V[m] == d_in_E[ix]) {
-                d_in_E[ix] = m;
+__global__ void update_INDEX_edgesGridStride(int N, int *d_in_V, int *d_in_E, int l, int r){
+    int ix = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = gridDim.x * blockDim.x;
+    int left = l;
+    int right = r;
+    for (int i=ix; i< N-1; i+=stride){
+        left = l;
+        right = r;
+        while (left <= right) {
+            int m = left + (right - left) / 2;
+            if (d_in_V[m] == d_in_E[i]) {
+                d_in_E[index] = m;
                 break;
             }
-            if (d_in_V[m] < d_in_E[ix]) {l = m + 1;} else {r = m - 1;}
-        }
-    }
-}
-
-__global__ void initialize(int N, int *p, int val, bool src, int source, int sourceVal){
-    int ix = threadIdx.x + (blockDim.x * blockIdx.x);
-    if (ix < N) {
-        p[ix] = val;
-        if(src){
-            if(ix == source) {
-                p[ix] = sourceVal;
+            if (d_in_V[m] < d_in_E[i]) {
+                left = m + 1;
+            } else {
+                right = m - 1;
             }
         }
     }
 }
 
-__global__ void update_results(int N, int *d_in_V, int *d_in_I, int *d_in_E, int *d_in_W, int *d_out_D, int *d_out_P){
-    int ix = threadIdx.x + blockDim.x * blockIdx.x;
-    if (ix < N) {
-        for (int j = d_in_I[ix]; j < d_in_I[ix+1]; ++j) {
-            int u = d_in_V[ix];
+__global__ void update_resultsGridStride(int N, int *d_in_V, int *d_in_I, int *d_in_E, int *d_in_W, int *d_out_D, int *d_out_P){
+    int ix = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = gridDim.x * blockDim.x;
+    for (int i=ix; i< N-1; i+=stride){
+        for (int j = d_in_I[i]; j < d_in_I[i+1]; ++j) {
+            int u = d_in_V[i];
             int w = d_in_W[j];
-            int dis_u = d_out_D[ix];
+            int dis_u = d_out_D[i];
             int dis_v = d_out_D[d_in_E[j]];
-            if (dis_v == dis_u + w) {atomicMin(&d_out_P[d_in_E[j]], u);}
+            if (dis_v == dis_u + w) {
+                atomicMin(&d_out_P[d_in_E[j]], u);
+            }
         }
     }
 }
@@ -104,22 +121,9 @@ int main(){
 //    load_data("/home/styagi/USA-road-d.NY.gr_W.csv", E);
 //    load_data("/home/styagi/USA-road-d.NY.gr_W.csv", W);
 
-//    CSVReader reader("/home/styagi/data/rand_1000.gr_V.csv");
-//    std::vector<int> V = reader.parsedata();
-//    CSVReader reader("/home/styagi/data/rand_1000.gr_I.csv");
-//    std::vector<int> I = reader.parsedata();
-//    CSVReader reader("/home/styagi/data/rand_1000.gr_E.csv");
-//    std::vector<int> E = reader.parsedata();
-//    CSVReader reader("/home/styagi/data/rand_1000.gr_W.csv");
-//    std::vector<int> W = reader.parsedata();
-
-    //cudaEvent_t start, stop;
-    //cudaEventCreate(&start);
-    //cudaEventCreate(&stop);
     auto start_time = high_resolution_clock::now();
     cout<< "loaded vectors from file now copying to gpu and executing sssp" << endl;
     cout<< V.size() <<endl;
-    //cudaEventRecord(start, 0);
 
     int threads_per_block = 128;
     int max_value = std::numeric_limits<int>::max();
@@ -134,9 +138,6 @@ int main(){
     cudaMalloc((void**) &d_in_I, I.size() *sizeof(int));
     cudaMalloc((void**) &d_in_E, E.size() *sizeof(int));
     cudaMalloc((void**) &d_in_W, W.size() *sizeof(int));
-
-    cout<< V.size() <<endl;
-
     cudaMalloc((void**) &d_out_D, V.size() *sizeof(int));
     cudaMalloc((void**) &d_out_Di, V.size() *sizeof(int));
     cudaMalloc((void**) &d_out_P, V.size() *sizeof(int));
@@ -151,24 +152,18 @@ int main(){
     initialize<<<init_blocks, threads_per_block>>>(V.size(), d_out_Di, max_value, true, 0, 0);
     initialize<<<init_blocks, threads_per_block>>>(V.size(), d_out_P, max_value, true, 0, 0);
 
-    int e_blocks = (E.size() + threads_per_block -1) / threads_per_block;
-    update_INDEX_edges<<<e_blocks, threads_per_block>>>(E.size(), d_in_V, d_in_E, 0, V.size()-1);
-
+    update_INDEX_edges<<<init_blocks, threads_per_block>>>(E.size(), d_in_V, d_in_E, 0, V.size()-1);
     for (int i=0; i<V.size(); ++i){
-        reset<<<blocks, threads_per_block>>>(N, max_value, d_in_V, d_in_I, d_in_E, d_in_W, d_out_D, d_out_Di);
-        update_distance<<<blocks, threads_per_block>>>(V.size(), d_in_V, d_in_I, d_in_E, d_in_W, d_out_D, d_out_Di);
+        resetGridStride()<<<init_blocks, threads_per_block>>>(N, max_value, d_in_V, d_in_I, d_in_E, d_in_W, d_out_D, d_out_Di);
+        update_distanceGridStride<<<blocks, threads_per_block>>>(V.size(), d_in_V, d_in_I, d_in_E, d_in_W, d_out_D, d_out_Di);
     }
-    update_results<<<blocks, threads_per_block>>>(V.size(), d_in_V, d_in_I, d_in_E, d_in_W, d_out_D, d_out_P);
+    update_resultsGridStride<<<blocks, threads_per_block>>>(V.size(), d_in_V, d_in_I, d_in_E, d_in_W, d_out_D, d_out_P);
 
-    //cudaEventRecord(stop, 0);
-    //cudaEventSynchronization(stop);
     cout << "finished executing sssp on cuda..." << endl;
     cudaDeviceSynchronize();
-    //float compute_time;
-    //cudaEventElapsedTime(&compute_time, start, stop);
     auto end_time = high_resolution_clock::now();
     auto exec_time = duration_cast<microseconds>(end_time - start_time);
-    cout << exec_time.count() << endl;
+    cout << exec_time.count() << "microseconds to complete" << endl;
 
     cudaFree(d_in_V);
     cudaFree(d_in_I);
@@ -177,4 +172,5 @@ int main(){
     cudaFree(d_out_D);
     cudaFree(d_out_Di);
     cudaFree(d_out_P);
+
 }
